380e5ed385164cfbaf84f5824aa46288704e6f3e
JoaoVictor809 <joaovictorleal.mello@outlook.com>
2025-02-03 14:55:10 -0300

    v3
    v3

---
+++ app/pages/main/_layout.tsx
@@ -0,0 +1,31 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+import {Tabs} from 'expo-router'
+import TabBar from '../../../components/TabBar'
+
+export default function _layout() {
+    return(
+        <Tabs
+        tabBar={props=> <TabBar {...props}/>}
+        >
+            <Tabs.Screen
+            name='index'
+            options={{
+                title: "Inicio"
+            }}
+            />
+            <Tabs.Screen
+            name='cursos'
+            options={{
+                title: "Cursos"
+            }}
+            />
+            <Tabs.Screen
+            name='provas'
+            options={{
+                title: "Provas"
+            }}
+            />
+        </Tabs>
+    )
+}
---
+++ app/pages/main/cursos.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from "react-native"
+import React from "react"
+
+export default function curso(){
+    return(
+        <View>
+            <Text>curso</Text>
+        </View>
+    )
+}
