1c745d50ae55c92e6b0a7fee9ea26964f9ce772f
JoaoVictor809 <joaovictorleal.mello@outlook.com>
2025-02-16 16:22:14 -0300

    Add files via upload

--- app/pages/main/provas.tsx
+++ app/pages/main/provas.tsx
@@ -1,17 +1,21 @@
-import {View, Text, TextInput, Pressable, Image} from "react-native"
-import React from "react"
-import Estilo from '../../../assets/style/provas'
-import { Link } from "expo-router";
+import { View, Text, TextInput, Pressable, Image, ScrollView, Dimensions,  } from "react-native";
+import React from "react";
+import Estilo from '../../../assets/style/provas';
 import { useFonts } from "expo-font";
 import { useState, useEffect } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
+import EnemLista from '../../../components/enemList';
+import UnicampLista from '../../../components/unicampList' 
+import BdbLista from '../../../components/bdbList'
 
-export default function prova(){
+const { height } = Dimensions.get('window');
+export default function prova() {
     const [text, onChangeText] = React.useState('');
     const [fontsLoaded] = useFonts({
         'Poppins_Regular': require('../../../assets/fonts/poppins/Poppins-Regular.ttf'),
         'Poppins_Bold': require('../../../assets/fonts/poppins/Poppins-Bold.ttf')
     });
+
     useEffect(() => {
         if (fontsLoaded) {
             SplashScreen.hideAsync();
@@ -19,26 +23,52 @@ export default function prova(){
     }, [fontsLoaded]);
 
     if (!fontsLoaded) {
-        // Retorna null ou qualquer outra coisa enquanto a tela de splash ainda está visível.
-        return null
+        return null;
     }
-    return(
-        <View style={Estilo.main}>
-            <View >
-                <Text style={Estilo.title}><h1>Provas</h1></Text>
+
+    return (
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={Estilo.main}>
+                <View style={Estilo.header}>
+                    <View>
+                        <Text style={Estilo.title}><h1>Provas</h1></Text>
+                    </View>
+                    <View style={Estilo.boxSearch}>
+                        <TextInput
+                            style={Estilo.input}
+                            onChangeText={onChangeText}
+                            value={text}
+                            placeholder="Pesquise o nome do curso"
+                            keyboardType="emailAddress" />
+                        <View style={Estilo.imgSearch}>
+                            <Pressable>
+                                <Image source={require('../../../assets/images/lupa.png')} />
+                            </Pressable>
+                        </View>
+                    </View>
+                </View>
+
+                <View style={[Estilo.containerMain, {height}]}>
+                   <View style={Estilo.containerEenm}>
+                    <ScrollView>
+                        <EnemLista />
+                    </ScrollView>
+
+                   </View>
+
+                   <View>
+                    <ScrollView>
+                        <UnicampLista />
+                    </ScrollView>
+                   </View>
+
+                   <View>
+                    <ScrollView>
+                        <BdbLista />
+                    </ScrollView>
+                   </View>
+                </View>
             </View>
-            <View>
-            <TextInput 
-                                style={Estilo.input}
-                                onChangeText={onChangeText}
-                                value={text}
-                                placeholder="Pesquise o nome do curso"
-                                keyboardType=".emailAddress" >
-                                    {/* <Pressable>
-                                    <Image source={require('../../../assets/images/lupa.png')} />
-                                    </Pressable> */}
-                                </TextInput>
-            </View>
-        </View>
-    )
+        </ScrollView>
+    );
 }
