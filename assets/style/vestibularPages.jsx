523b34c23c989759cdd2b1e244770999dc19d4c0
JoaoVictor809 <joaovictorleal.mello@outlook.com>
2025-03-13 22:22:27 -0300

    Add files via upload

--- assets/style/vestibularPages.jsx
+++ assets/style/vestibularPages.jsx
@@ -78,5 +78,17 @@ export default StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         gap: 10
-    }
+    },
+   
+        containerBox: {
+          flex: 1,
+          backgroundColor: 'grey',
+        },
+        contentContainer: {
+          flex: 1,
+          padding: 36,
+          alignItems: 'center',
+        },
+     
+      
 });
