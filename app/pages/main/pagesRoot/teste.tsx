f5a91e33f9998e26b33a473ee835336ca9c2f2c5
JoaoVictor809 <joaovictorleal.mello@outlook.com>
2025-03-14 20:31:41 -0300

    Add files via upload

--- app/pages/main/pagesRoot/teste.tsx
+++ app/pages/main/pagesRoot/teste.tsx
@@ -1,17 +1,19 @@
 import React, { useCallback, useMemo, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import { GestureHandlerRootView, Pressable } from 'react-native-gesture-handler';
 import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { LinearGradient } from 'expo-linear-gradient';
+import CustomBackground from '@/components/CustomBackground'; 
+import MenuDrop from '../../../../components/menuDrop'
 
 SplashScreen.preventAutoHideAsync();
 
 const App = () => {
-  // ref
+  // refs e snapPoints
   const bottomSheetRef = useRef<BottomSheet>(null);
-  const snapPoints = useMemo(() => ["30%","50%", "80%"], [])
+  const snapPoints = useMemo(() => ['30%', '50%', '80%'], []);
 
   const hqandleOpenPress = () => bottomSheetRef.current?.expand();
   const hqandleClosePress = () => bottomSheetRef.current?.close();
@@ -24,40 +26,42 @@ const App = () => {
   const [fontsLoaded] = useFonts({
     'Poppins_Regular': require('../../../../assets/fonts/poppins/Poppins-Regular.ttf'),
     'Poppins_Bold': require('../../../../assets/fonts/poppins/Poppins-Bold.ttf'),
-});
+  });
 
-useEffect(() => {
+  useEffect(() => {
     if (fontsLoaded) {
-        SplashScreen.hideAsync();
+      SplashScreen.hideAsync();
     }
-}, [fontsLoaded]);
+  }, [fontsLoaded]);
 
-if (!fontsLoaded) {
+  if (!fontsLoaded) {
     return null;
-}
+  }
 
-
-  // renders
   return (
     <GestureHandlerRootView style={styles.container}>
-        <Button title='Abrir' onPress={hqandleOpenPress}/>
+      <Button title="Abrir" onPress={hqandleOpenPress} />
+        <MenuDrop />
       <BottomSheet
         ref={bottomSheetRef}
         index={-1}
         onChange={handleSheetChanges}
         snapPoints={snapPoints}
         enablePanDownToClose={true}
+        backgroundComponent={(props) => <CustomBackground {...props} />}
+        style={styles.bottomSheet} 
       >
-        <LinearGradient
-        style={{flex:1, width:'100%'}}
-        colors={[ '#FFFFFF', '#CDDDF6','#1261D7']}>
-            <BottomSheetView style={styles.contentContainer}>
-          <Text style={styles.tiltle}>Informações</Text>
-          <Button title='gerar' onPress={() => {}}/>
-          <Button title='fechar' onPress={hqandleClosePress}/>
+        <BottomSheetView style={styles.contentContainer}>
+          <View style={styles.containerTitle}>
+            <Text style={styles.title}>Informações</Text>
+            <Pressable onPress={hqandleClosePress} >
+                <Image source={require('@/assets/images/icons/close.png')}/>
+            </Pressable>
+          </View>
+          
+          <Button title="gerar" onPress={() => {}} />
+          <Button title="fechar" />
         </BottomSheetView>
-        </LinearGradient>
-        
       </BottomSheet>
     </GestureHandlerRootView>
   );
@@ -73,11 +77,23 @@ const styles = StyleSheet.create({
     padding: 36,
     alignItems: 'center',
   },
-tiltle:{
-    fontSize:30,
-    fontFamily:'Poppins_Bold',
-
-}
+  title: {
+    fontSize: 30,
+    fontFamily: 'Poppins_Bold',
+    color:'#fff'
+  },
+  bottomSheet: {
+    borderTopLeftRadius: 40, 
+    borderTopRightRadius: 40, 
+    overflow: 'hidden', 
+  },
+  containerTitle:{
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'space-around',
+    alignItems:'center',
+    width:'100%'
+  }
 });
 
 export default App;
