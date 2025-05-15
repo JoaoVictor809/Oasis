cb003bb3c6c71baabb6554a66eaf7a540d463a6f
vitoriasiqe <maximianovitoria@hotmail.com>
2025-05-14 21:23:22 -0300

    Add files via upload
    correção do erro outlinewidth

--- app/pages/main/cursos.tsx
+++ app/pages/main/cursos.tsx
@@ -1,22 +1,30 @@
-import { View, Text, TextInput, Pressable, Image, ScrollView, Dimensions, ImageBackground, StyleSheet } from "react-native"
-import React from "react"
+import {
+    View,
+    Text,
+    TextInput,
+    Pressable,
+    Image,
+    ScrollView,
+    Dimensions,
+    ImageBackground,
+    StyleSheet
+} from "react-native";
+import React, { useState, useEffect } from "react";
 import { useFonts } from "expo-font";
-import { useState, useEffect } from 'react';
-import * as SplashScreen from 'expo-splash-screen';
-import Estilo from '../../../assets/style/curso'
+import * as SplashScreen from "expo-splash-screen";
+import Estilo from "../../../assets/style/curso";
 import Slider from "@/components/Slider";
-import { ImageSlider } from '@/data/SliderData'
-import ListCurso001 from '@/components/curso/curso001'
-import Icon from 'react-native-vector-icons/FontAwesome6'; 
+import { ImageSlider } from "@/data/SliderData";
+import ListCurso001 from "@/components/curso/curso001";
+import Icon from "react-native-vector-icons/FontAwesome6";
 
-//pega a altura 
-const { height } = Dimensions.get('window');
+const { height } = Dimensions.get("window");
+
 export default function curso() {
-
-    const [text, onChangeText] = React.useState('');
+    const [text, onChangeText] = useState("");
     const [fontsLoaded] = useFonts({
-        'Poppins_Regular': require('../../../assets/fonts/poppins/Poppins-Regular.ttf'),
-        'Poppins_Bold': require('../../../assets/fonts/poppins/Poppins-Bold.ttf')
+        Poppins_Regular: require("../../../assets/fonts/poppins/Poppins-Regular.ttf"),
+        Poppins_Bold: require("../../../assets/fonts/poppins/Poppins-Bold.ttf")
     });
 
     useEffect(() => {
@@ -28,35 +36,38 @@ export default function curso() {
     if (!fontsLoaded) {
         return null;
     }
-    return (
 
+    return (
         <ScrollView showsVerticalScrollIndicator={false}>
             <View style={Estilo.formaImg}>
                 <ImageBackground
-                    source={require('@/assets/images/formas/formaCurso001.png')} />
+                    source={require("@/assets/images/formas/formaCurso001.png")}
+                />
             </View>
+
             <View style={Estilo.main}>
                 <View style={Estilo.header}>
                     <View>
-                        <Text style={Estilo.title}><h1>Cursos</h1></Text>
+                        <Text style={Estilo.title}>Cursos</Text>
                     </View>
+
                     <View style={Estilo.boxSearch}>
                         <TextInput
-                            style={[Estilo.input, { outlineWidth: 0 }]} 
+                            style={Estilo.input}
                             onChangeText={onChangeText}
                             value={text}
                             placeholder="Pesquise o nome do curso"
-                            keyboardType="email-address"  
+                            keyboardType="default"
                         />
                         <View style={Estilo.imgSearch}>
                             <Pressable>
-                               <Icon name="magnifying-glass" solid color={'#7F8FAF'} size={27}/>
+                                <Icon name="magnifying-glass" solid color={"#7F8FAF"} size={27} />
                             </Pressable>
                         </View>
                     </View>
                 </View>
-                <View style={[Estilo.containerMain, { height }]}>
 
+                <View style={[Estilo.containerMain, { height }]}>
                     <View style={styles.Container}>
                         <Slider itemList={ImageSlider} />
                     </View>
@@ -64,19 +75,14 @@ export default function curso() {
                     <View>
                         <ListCurso001 />
                     </View>
-
-
                 </View>
-
             </View>
         </ScrollView>
-
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     Container: {
-
         paddingTop: 60
     }
-})
+});
